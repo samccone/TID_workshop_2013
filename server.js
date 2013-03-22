fac7d50c765ae3a7d7420901afc94f4001465057
Sam Saccone <samccone@gmail.com>
2013-03-22 00:13:36 -0400

    add a feed page to TID

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var server  = express();
-var port = process.env.port || 1111;
+var port    = process.env.port || 1111;
+var request = require('request');
 
 server.use(express.static(__dirname + '/public'));
+server.set('views', __dirname + "/views");
+server.set("view engine", "jade");
 
-server.get('*', function(req, res) {
+server.get('/', function(req, res) {
   res.redirect('/');
+});
+
+server.get('/feed', function(req, res) {
+  request.get('https://api.instagram.com/v1/tags/tidworkshop/media/recent?access_token='+process.env['INSTAGRAM_TOKEN'], function(error, response, d) {
+    res.render('feed', {d: JSON.parse(d)});
+  });
 });
 
 server.listen(port);
